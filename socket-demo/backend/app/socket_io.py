a7b38f59b902daee241c81f13c65cac02221222d
Vernyoon Chao <vernchao510@gmail.com>
2022-05-25 20:25:46 -0700

    finished demoish

--- socket-demo/backend/app/socket_io.py
+++ socket-demo/backend/app/socket_io.py
@@ -1,7 +1,8 @@
 from flask_socketio import SocketIO, emit
 
-socketio = SocketIO(cors_allowed_origins=["*"])
+socketio = SocketIO(cors_allowed_origins="*")
 
 @socketio.on("chat")
 def handle_chat(data):
-    emit("chat",data, broadcast=True)
+    print(data)
+    emit("chat", data, broadcast=True)
--- socket-demo/frontend/src/components/chat.js
+++ socket-demo/frontend/src/components/chat.js
@@ -1,52 +1,49 @@
-import {io} from 'socket.io-client';
+import { io } from "socket.io-client";
 import React, { useState, useEffect } from "react";
 // import { useSelector } from "react-redux";
 
 let socket;
 
 const Chat = () => {
-    const [messages, setMessages] = useState([]);
-    const [chatInput, setChatInput] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [chatInput, setChatInput] = useState("");
 
-    useEffect(() => {
-        socket = io();
+  useEffect(() => {
+    socket = io();
 
-        socket.on("chat", (chat) => {
-            setMessages(messages => [...messages, chat])
-        })
+    socket.on("chat", (data) => {
+      console.log(data);
+      setMessages((messages) => [...messages, data]);
+    });
 
-        return (()=> {
-            socket.disconnect()
-        })
-    },[])
-
-    const updateChatInput = (e) => {
-        setChatInput(e.target.value)
+    return () => {
+      socket.disconnect();
     };
+  }, []);
 
-    const sendChat = (e) => {
-        e.preventDefault()
-        socket.emit("chat", {user: "Darren", msg: chatInput});
-        setChatInput("")
+  const updateChatInput = (e) => {
+    setChatInput(e.target.value);
+  };
 
-    }
-    return (
-        <div>
-            <form onSubmit={sendChat}>
-                <input
-                value={chatInput}
-                onChange={updateChatInput}
-                />
-                <button type="submit">Send</button>
-            </form>
-            <div>
-                {messages.map((message,ind)=>(
-                    <div key={ind}>{`${message.user}: ${message.msg}`}</div>
-                ))}
-            </div>
-        </div>
+  const sendChat = (e) => {
+    e.preventDefault();
+    socket.emit("chat", { user: "Darren", msg: chatInput });
+    setChatInput("");
+  };
 
-    )
-}
+  return (
+    <div>
+      <form onSubmit={sendChat}>
+        <input value={chatInput} onChange={updateChatInput} />
+        <button type="submit">Send</button>
+      </form>
+      <div>
+        {messages.map((message, ind) => (
+          <div key={ind}>{`${message.user}: ${message.msg}`}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
