40320d60f5b64c2775b672ba63157b4e7cfb3ceb
Chris Threadgill <christhreadgill@live.com>
2022-06-01 13:01:09 -0500

    channel contents display css bug fixed

--- react-app/src/components/ServerDisplay/ServerDisplay.js
+++ react-app/src/components/ServerDisplay/ServerDisplay.js
@@ -55,6 +55,18 @@ function ServerDisplay() {
               <ServerUserCard></ServerUserCard>
               <ServerUserCard></ServerUserCard>
               <ServerUserCard></ServerUserCard>
+              <ServerUserCard></ServerUserCard>
+              <ServerUserCard></ServerUserCard>
+              <ServerUserCard></ServerUserCard>
+              <ServerUserCard></ServerUserCard>
+              <ServerUserCard></ServerUserCard>
+              <ServerUserCard></ServerUserCard>
+              <ServerUserCard></ServerUserCard>
+              <ServerUserCard></ServerUserCard>
+              <ServerUserCard></ServerUserCard>
+              <ServerUserCard></ServerUserCard>
+              <ServerUserCard></ServerUserCard>
+              <ServerUserCard></ServerUserCard>
               <div>OFFLINE</div>
               <ServerUserCard></ServerUserCard>
               <ServerUserCard></ServerUserCard>
