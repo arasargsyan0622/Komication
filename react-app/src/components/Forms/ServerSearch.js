2de647e63be3477fba5be821a2ea4b221e42dcd4
Chris Threadgill <christhreadgill@live.com>
2022-06-01 05:10:15 -0500

    server/channel/online users display complete

---
+++ react-app/src/components/Forms/ServerSearch.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+import "./NonAuthFormsCSS/ServerSearch.css";
+
+function ServerSearch() {
+  const [searchActive, setSearchActive] = useState(false);
+  return (
+    <div className="server__search__form">
+      <form>
+        <input
+          onFocus={() => setSearchActive(true)}
+          onBlur={() => setSearchActive(false)}
+          className={searchActive ? "server__search__active" : "server__search__inactive"}
+          type="search"
+          placeholder="Search"
+        ></input>
+      </form>
+    </div>
+  );
+}
+
+export default ServerSearch;
--- react-app/src/components/ServerDisplay/ServerChannelNav/ServerChannelList/ServerChannelList.js
+++ react-app/src/components/ServerDisplay/ServerChannelNav/ServerChannelList/ServerChannelList.js
@@ -7,9 +7,7 @@ import { useState, useEffect } from "react";
 function ServerChannelList() {
   const currentServer = useSelector((state) => state.current_server);
   console.log(currentServer, "hello");
-  const [channels, setChannels] = useState(
-    Object.values(currentServer)[0]?.channels
-  );
+  const [channels, setChannels] = useState(Object.values(currentServer)[0]?.channels);
 
   const uuid = Object.values(currentServer)[0]?.server.server_invite_url;
 
@@ -20,21 +18,15 @@ function ServerChannelList() {
   return (
     <div className="server__channel__list__container">
       <div>
-        <NavLink
-          to="/servers/serverId/edit"
-          className={"server__channel__header"}
-        >
+        <NavLink to="/servers/serverId/edit" className={"server__channel__header"}>
           TEXT CHANNELS
         </NavLink>
       </div>
 
       {channels?.map((channel) => {
         return (
-          <NavLink
-            className={"server__channel__link"}
-            to={`/servers/${uuid}/channelId`}
-          >
-            {channel.channel_name}
+          <NavLink key={channel.id} className={"server__channel__link"} to={`/servers/${uuid}/${channel.channel_uuid}`}>
+            # {channel.channel_name}
           </NavLink>
         );
       })}
