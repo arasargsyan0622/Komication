65376fc11af44881fd0f9f4dc08ff040c77323b7
Vernyoon Chao <vernchao510@gmail.com>
2022-06-03 10:09:21 -0700

    work in progress

--- react-app/src/components/ServerDisplay/ServerChannelNav/ChannelRightSide/ChannelRightSide.js
+++ react-app/src/components/ServerDisplay/ServerChannelNav/ChannelRightSide/ChannelRightSide.js
@@ -7,6 +7,8 @@ import ServerUserCard from "../../../ServerUserCard/ServerUserCard";
 import NoTextChannel from "../../../NoTextChannel/NoTextChannel";
 import "../../../ServerDisplay/ServerDisplay.css";
 
+import UserHomeLoadingScreen from "../../../LoadingScreens/UserHomeLoadingScreen";
+
 function ChannelRightSide() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
@@ -20,44 +22,55 @@ function ChannelRightSide() {
   // console.log(currentServer.server.users);
 
   useEffect(() => {
-    setIsLoaded(true);
+    let mounted = true;
+    let t = setTimeout(() => {
+      if (mounted) {
+        setIsLoaded(true);
+      }
+    }, 1500);
   }, [dispatch, channel]);
 
-  return (
-    isLoaded && (
-      <div className="channel__right__side__container">
-        {currentChannel ? (
-          <div className="server__channel__display">
-            <div className="server__header__nav">
-              <div className="server__header__name">
-                <div className="server__header__hash"></div>
-                <span>{currentChannel?.channel.channel_name}</span>
-              </div>
-              <div className="server__search__container">
-                <ServerSearch></ServerSearch>
-              </div>
-            </div>
-            <div className="server__channel__display__container">
-              <ChannelDisplay></ChannelDisplay>
-              <div className="online__users__container">
-                <div>ONLINE — 0</div>
-                <div>OFFLINE — {currentServer.server.users.length}</div>
-                {currentServer.server.users.map((user) => {
-                  return (
-                    <div>
-                      <ServerUserCard user={user} key={user}></ServerUserCard>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          </div>
-        ) : (
-          <NoTextChannel />
-        )}
-      </div>
-    )
+  return isLoaded ? (
+    <> {currentServer ? <h1>hello</h1> : <NoTextChannel />} </>
+  ) : (
+    <UserHomeLoadingScreen />
   );
+
+  // isLoaded ? (
+  // <div className="channel__right__side__container">
+  //   {currentChannel ? (
+  //     <div className="server__channel__display">
+  //       <div className="server__header__nav">
+  //         <div className="server__header__name">
+  //           <div className="server__header__hash"></div>
+  //           <span>{currentChannel?.channel.channel_name}</span>
+  //         </div>
+  //         <div className="server__search__container">
+  //           <ServerSearch></ServerSearch>
+  //         </div>
+  //       </div>
+  //       <div className="server__channel__display__container">
+  //         <ChannelDisplay></ChannelDisplay>
+  //         <div className="online__users__container">
+  //           <div>ONLINE — 0</div>
+  //           <div>OFFLINE — {currentServer.server.users.length}</div>
+  //           {currentServer.server.users.map((user) => {
+  //             return (
+  //               <div>
+  //                 <ServerUserCard user={user} key={user}></ServerUserCard>
+  //               </div>
+  //             );
+  //           })}
+  //         </div>
+  //       </div>
+  //     </div>
+  //     ) : (
+  //       <NoTextChannel />
+  //     )}
+  //   </div>
+  // ) : (
+  //   <UserHomeLoadingScreen />
+  // );
 }
 
 export default ChannelRightSide;
