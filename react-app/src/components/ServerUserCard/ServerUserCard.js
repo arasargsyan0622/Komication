ccea7911c950e26bcb4c07b19e33ee5d838d9545
Vernyoon Chao <vernchao510@gmail.com>
2022-06-03 03:30:01 -0700

    user list

--- react-app/src/components/ServerUserCard/ServerUserCard.js
+++ react-app/src/components/ServerUserCard/ServerUserCard.js
@@ -3,10 +3,11 @@ import React from "react";
 import "./ServerUserCard.css";
 
 function ServerUserCard({ user }) {
+  console.log(user);
   return (
     <div className="server__user__card__container">
       <div className="server__user__card__avatar"></div>
-      <div className="server__user__card__name">user.UserName</div>
+      <div className="server__user__card__name">{user.username}</div>
     </div>
   );
 }
