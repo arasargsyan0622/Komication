a1c935cb72478b57bbbb980d276e42ace860a8eb
ara <arasargsyan0531@gmail.com>
2022-06-04 02:58:13 -0700

    inboxes add, need to show

--- react-app/src/components/UserHome/Inbox/InboxMessageList/InboxMessageList.js
+++ react-app/src/components/UserHome/Inbox/InboxMessageList/InboxMessageList.js
@@ -1,14 +1,41 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from 'react-redux'
+import { getCurrentUserInboxes, addCurrentUserInbox } from "../../../../store/direct_messages"
+
 
 import "./InboxMessageList.css";
 
 function InboxMessageList() {
-  const [setIsLoaded] = useState(false);
+  const dispatch = useDispatch()
 
-  // query all inbox channels/'messages'
-  // const servers = Object.values(useSelector((state) => state.servers));
+  const [ isLoaded, setIsLoaded ] = useState(false);
+  const [ newUser, setNewUser ] = useState()
+  const currInbox = Object.values(useSelector((state) => state.current_inboxes))
+  const userId = useSelector((state) => state.session.user.id)
+  // const inboxUuid = Object.values(currInbox[0]).map((inbox) => {
+  //   if (inbox) {
+  //     return inbox?.inbox_uuid
+  //   }
+  // })
+  // console.log("---------", inboxUuid)
+
+    useEffect(() => {
+        dispatch(getCurrentUserInboxes(userId)).then(() => setIsLoaded(true))
+    }, [dispatch])
+
+    const addInboxChannel = ((e)=>{
+        e.preventDefault()
+
+        const payload = {
+            userId,
+            newUser,
+        }
+        dispatch(addCurrentUserInbox(payload))
+        setNewUser("")
+    })
 
   return (
+    isLoaded && (
     <div className="inbox__message__list__container">
       <div className="inbox__messages__headers">
         <div className="inbox__messages__header__container">
@@ -22,141 +49,26 @@ function InboxMessageList() {
       </div>
       <div className="direct__message__container">
         <div className="direct__message__header__container">
-          <div className="direct__message__header">DIRECT MESSAGES</div>
-          <button className="new__direct__message__button"></button>
+          <form onSubmit={addInboxChannel}>
+              <div className="direct__message__header">DIRECT MESSAGES</div>
+              <input onChange={(e)=> setNewUser(e.target.value)} placeholder="Enter username"></input>
+              <button className="new__direct__message__button" type="submit"></button>
+          </form>
         </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
-        <div className="direct__message__card">
-          <div className="direct__message__avatar"></div>
-          <div className="direct__message__username">UserName</div>
-          <button className="direct__message__hide"></button>
-        </div>
+        <>
+          {Object.values(currInbox[0]).map((inbox) => {
+              return (
+                  <div key={inbox.id}>
+                      <div>uuid: {inbox.inbox_uuid}</div>
+                      <div className="direct__message__avatar"></div>
+                      <div className="direct__message__username">{inbox.users.username}</div>
+                  </div>
+              )
+          })}
+        </>
       </div>
     </div>
+    )
   );
 }
 
