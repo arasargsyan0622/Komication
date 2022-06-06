e9a749883f7e02f38b035ea541cf115b2bbd304d
ara <arasargsyan0531@gmail.com>
2022-06-06 08:18:52 -0700

    404 page

--- react-app/src/components/404/404.js
+++ react-app/src/components/404/404.js
@@ -1,14 +1,22 @@
 import React from "react";
 import "./404.css"
+import { useEffect } from "react"
+import { useHistory } from "react-router-dom";
 
 function ErrorPage() {
+    const history = useHistory()
+    useEffect(() => {
+        setTimeout(() => {
+            history.push("/me")
+        }, 5000)
+    }, [])
     return (
         <div className="container">
-        <h1 className="header">
-            Sorry, page requested could not be found!
-            <p className="second">Check your URL</p>
-            <div className="img"></div>
-        </h1>
+            <h1 className="header">
+                Sorry, page requested could not be found!
+                <p className="second">Check your URL</p>
+                <div className="img"></div>
+            </h1>
         </div>
     )
 }
